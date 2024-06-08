23908f46b0941fcd460874f76786e2f093f24d85
Nicolle Mello <nimellodev@gmail.com>
2024-06-07 22:30:46 -0300

    update: styles signup

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,14 +1,17 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import {postUser} from '../../services/userApi/index.jsx'
 import styles from './styles.module.css'
 import { useNavigate } from 'react-router-dom';
+import { HeaderRegister } from '../../components/Header';
+import { Footer } from '../../components/Footer';
 
 export const SignUp = () => {
   const [userData, setUserData] = useState({
     name: '',
     email: '',
     password: ''
-  }) 
+  })
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -25,43 +28,52 @@ export const SignUp = () => {
     console.log(userData)
     postUser(userData)
     navigate('/login')
-
-      
   };
 
   return (
     <div className={styles.containerPrincipal}>
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit} className={styles.signupForm}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={userData.name}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email" 
-            name="email"
-            value={userData.email}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={userData.password}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit">Sign Up</button>
-      </form>
+      <HeaderRegister />
+
+      <section className={styles.body}>
+
+        <form onSubmit={handleSubmit} className={styles.signupForm}>
+        <h1>Registrar</h1>
+
+          <label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Nome"
+              value={userData.name}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={userData.email}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Senha"
+              value={userData.password}
+              onChange={handleChange}
+            />
+          </label>
+
+          <button type="submit">Sign Up</button>
+        </form>
+      </section>
+
+      <Footer />
     </div>
   );
 };
