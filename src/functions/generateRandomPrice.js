238ef941207462d0016ae200ee44bce376d22ae2
Heitor Bittencourt <heitorsbittencourt@gmail.com>
2024-06-11 18:07:07 -0300

    update: função generateRandomPrice e nomes dos cards

--- src/functions/generateRandomPrice.js
+++ src/functions/generateRandomPrice.js
@@ -1,6 +1,11 @@
 export function generateRandomPrice() {
     const min = 0.01;
-    const max = 0.5;
+    const max = 1.0;
+    return (Math.random() * (max - min) + min).toFixed(2) + ' ETH';
+}
+export function generateRandomPriceLendarias() {
+    const min = 1.01;
+    const max = 2.0;
     return (Math.random() * (max - min) + min).toFixed(2) + ' ETH';
 }
 
